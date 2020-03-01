9898cc56c453e93443b821381dd4ef9aa237fbe1
Con-Fou <52680242+Con-Fou@users.noreply.github.com>
2020-03-01 12:58:38 +0200

    Initialize sensors

--- DataLogger/main.ino
+++ DataLogger/main.ino
@@ -5,12 +5,37 @@ Here goes the arduino code
 */ 
 
 // #include <iostream>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
 #include <rocket.h>
+#include <defaults.h>
 
+Adafruit_BME280 bme;
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 void setup(){
+	Wire.begin(); // Init I2C bus
+	Serial.begin(115200); // Init serial bus
+	
+	// Initialize the sensors
+    if (! bme.begin(0x77, &Wire)) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1) delay(10);
+    }
 
+    if(!bno.begin()){
+		/* There was a problem detecting the BNO055 ... check your connections */
+		Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+		while(1);
+	}
+	
+	delay(1000);
+
+	bno.setExtCrystalUse(true);
 }
 void loop(){
 
